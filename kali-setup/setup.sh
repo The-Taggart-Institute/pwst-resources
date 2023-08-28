12efeec740f426ea0ff5a964c15e2aa97a48c2bc
Michael Taggart <mtaggart@taggart-tech.com>
2023-08-28 16:24:39 -0700

    remove unnecessary mkdir

--- kali-setup/setup.sh
+++ kali-setup/setup.sh
@@ -33,7 +33,6 @@ git clone https://github.com/danielmiessler/SecLists
 echo "Downloading NerdFont"
 wget -O ~/tmp/scp.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/SourceCodePro.zip
 echo "Installing NerdFont"
-mkdir /tmp/scp
 unzip /tmp/scp.zip -d /tmp/scp '*.ttf'
 sudo mkdir /usr/share/fonts/saucecode-pro
 sudo mv /tmp/*.ttf /usr/share/fonts/saucecode-pro
